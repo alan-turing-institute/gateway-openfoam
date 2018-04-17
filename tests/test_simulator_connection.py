2e33afdfc62c908685ed93a53a26b9d3b319052a
Nick Barlow <nbarlow@turing.ac.uk>
2018-04-17 10:16:43 +0100

    updating tests

--- tests/test_simulator_connection.py
+++ tests/test_simulator_connection.py
@@ -2,10 +2,30 @@
 Test the simulator connection.
 """
 
+import sys
+sys.path[0:0] = ["gateway-job-manager-openfoam"]
+import config
+from connection import simulator
+
+class DummyCredentials(object):
+    """
+    mimic a flask app's config object - allow 
+    attributes to be retrieved via get()
+    """
+    def __init__(self,config):
+        self.config = config
+    def get(self, attr):
+        return self.config.__getattribute__(value)
+
+
 #from connection.simulator import Connection
 
 def test_simulator_connection():
- #   connection = Connection()
+    my_config = config.DevelopmentConfig()
+    credentials = DummyCredentials(my_config)
+    con = simulator.SimulatorConnection(credentials)
+    return con
+    #   connection = Connection()
     assert(True)
     # out, err, exit_code = connection._run_remote_command('echo hello')
     # print(out, err, exit_code)
