1c8287841abf02f47b5e1b95947db342d858c210
nbarlowATI <nbarlow@turing.ac.uk>
2018-04-17 11:57:45 +0100

    fix simulator connection test

--- tests/test_simulator_connection.py
+++ tests/test_simulator_connection.py
@@ -2,31 +2,47 @@
 Test the simulator connection.
 """
 
+import time
+import os
+
 import sys
 sys.path[0:0] = ["gateway-job-manager-openfoam"]
 import config
 from connection import simulator
 
-class DummyCredentials(object):
+class DummyConfig(object):
     """
     mimic a flask app's config object - allow 
     attributes to be retrieved via get()
     """
     def __init__(self,config):
+        """
+        constructor takes an instance of one of the classes in config.py
+        just need to modify where the simulator_key is found
+        """
         self.config = config
-    def get(self, attr):
-        return self.config.__getattribute__(value)
+        self.config.SSH_PRIVATE_KEY_PATH = 'gateway-job-manager-openfoam/keys/simulator_key'
+        
+    def get(self, attribute):
+        """
+        return an attribute if it is in the config dictionary, otherwise None.
+        """
+        if attribute in self.config.__dir__():
+            return self.config.__getattribute__(attribute)
+        else:
+            return None
 
-
-#from connection.simulator import Connection
-
+        
 def test_simulator_connection():
-    my_config = config.DevelopmentConfig()
-    credentials = DummyCredentials(my_config)
-    con = simulator.SimulatorConnection(credentials)
-    return con
-    #   connection = Connection()
-    assert(True)
-    # out, err, exit_code = connection._run_remote_command('echo hello')
-    # print(out, err, exit_code)
-    # print(connection.hostname)
+    """
+    use the credentials in gateway-job-manager/config.py to ssh to simulator
+    test that we can get the simulator to echo 'hello'
+    """
+    dev_config = config.DevelopmentConfig()
+    dummy_config = DummyConfig(dev_config)
+    credentials = simulator.SSH_Credentials(dummy_config)
+    connect = simulator.SimulatorConnection(credentials)
+    out, err, exit_code = connect._run_remote_command('echo hello')
+    print(out, err, exit_code)
+    assert(out.strip() == "hello")
+
