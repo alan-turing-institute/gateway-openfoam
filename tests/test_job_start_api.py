98e9c21dd23327cf9bef18883f2c5c7612276b39
nbarlowATI <nbarlow@turing.ac.uk>
2018-04-30 09:49:29 +0100

    test we can execute a remote script

--- tests/test_job_start_api.py
+++ tests/test_job_start_api.py
@@ -29,14 +29,39 @@ job_data = {
     "fields_to_patch": [ ]
 }
 
-def test_job_start_api():
+def test_copy_scripts():
     """
     try to start a job
     """
     # wait a bit for the service to come up
     time.sleep(5)
-    r = requests.post(JOB_MANAGER_URL+"/job/12345/start",json=job_data)
+    r = requests.post(JOB_MANAGER_URL+"/job/125/start",json=job_data)
     print(r.content)
     rjson = json.loads(r.content.decode("utf-8"))
     assert(rjson["status"] == 0)
 
+
+job_data_with_run = {
+    "scripts" : [
+      {
+          "source" : "https://sgmiddleware.blob.core.windows.net/testopenfoamapi/test_cmd.sh" ,
+          "action" : "RUN",
+          "destination" : "test_cmd.sh",
+          "patch" : False
+      }
+    ],
+    "username" : "testuser",
+    "fields_to_patch": [ ]
+}
+
+def test_run_cmd():
+    """
+    try to start a job
+    """
+    # wait a bit for the service to come up
+    time.sleep(5)
+    r = requests.post(JOB_MANAGER_URL+"/job/98765/start",json=job_data_with_run)
+    print(r.content)
+    rjson = json.loads(r.content.decode("utf-8"))
+    print(rjson["stdout"])
+    assert(rjson["status"] == 0)
